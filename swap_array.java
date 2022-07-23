d1dbd5c46b6372981eb0e8138818e13550d0b9e0
RajavJain <rajavjain2002@gmail.com>
2022-07-23 19:29:22 +0530

    Comments added

--- swap_array.java
+++ swap_array.java
@@ -5,11 +5,11 @@ public class swap_array {
     {
         int [] arr = {21,232,453,54,756,65};
         int l=arr.length;
-        int n=Math.floorDiv(l, 2);
+        int n=Math.floorDiv(l, 2);//ye hamko integer no dega jo bhi divide hokar bachega.
         int temp;
         for(int i=0;i<n;i++)
         {
-            temp=arr[i];
+            temp=arr[i];// temp isliye banaya hai taaki jo pehla no hoga wo phele temo mai store hoga then last wla 1st mai chala jaaega and temp wala last mai similarly eshe hi saare respectively honge.
             arr[i]=arr[l-i-1];
             arr[l-i-1]=temp;
         }
